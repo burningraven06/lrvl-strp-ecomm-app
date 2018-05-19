619f4d656e34aba2fd2f11794d14831616b3f2c6
Toufiq <burningraven06@gmail.com>
2018-05-19 17:17:17 +0600

    show output from db

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -13,249 +13,36 @@
     <h1> Shop </h1>
   </div>
 
-  <div class="row">
-    <div class="col s12 m4 mt16">
-      <div class="card">
-        <div class="card-image">
-          <img class='responsive-img' src="{{ asset('images/dummy_product_2.jpg')}}">
-          {{-- <span class="card-title">Card Title</span> --}}
-        </div>
-        <div class="card-content">
-          <h5> Card Title</h5>
-          <p>I am a very simple card. I am good at containing small bits of information.</p>
-        </div>
-        <div class="card-action ">
-          <div class='row mb0'>
-            <div class='col s6'>
-              <h5 class='center-align'> <em>$15.99</em></h5>
+  @foreach($products->chunk(3) as $productChunk)
+    <div class='row'>
+      @foreach($productChunk as $product)
+        <div class="col s12 m4 mt16">
+          <div class="card">
+            <div class="card-image">
+              <img class='responsive-img' src= "{{ $product->imgPath }}" title=" {{ $product->title}}" />
             </div>
-            <div class='col s6'>
-              <h5 class='center-align'>
-                <a href="#" title='Add to Cart'>
-                  <i class='material-icons cust_fz36'>add_shopping_cart</i>
-                </a>
-              </h5>
+            <div class="card-content">
+              <h5> {{ $product->title }}</h5>
+              <p> {{ substr($product->description, 0, 60) }} ...</p>
             </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="col s12 m4 mt16">
-      <div class="card">
-        <div class="card-image">
-          <img class='responsive-img' src="{{ asset('images/dummy_product_1.jpg')}}">
-          {{-- <span class="card-title">Card Title</span> --}}
-        </div>
-        <div class="card-content">
-          <h5> Card Title</h5>
-          <p>I am a very simple card. I am good at containing small bits of information.</p>
-        </div>
-        <div class="card-action ">
-          <div class='row mb0'>
-            <div class='col s6'>
-              <h5 class='center-align'> <em>$15.99</em></h5>
-            </div>
-            <div class='col s6'>
-              <h5 class='center-align'>
-                <a href="#" title='Add to Cart'>
-                  <i class='material-icons cust_fz36'>add_shopping_cart</i>
-                </a>
-              </h5>
+            <div class="card-action ">
+              <div class='row mb0'>
+                <div class='col s6'>
+                  <h5 class='center-align'> <em> {{ $product->price}}</em></h5>
+                </div>
+                <div class='col s6'>
+                  <h5 class='center-align'>
+                    <a href="#" title='Add to Cart'>
+                      <i class='material-icons cust_fz36'>add_shopping_cart</i>
+                    </a>
+                  </h5>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      @endforeach
     </div>
-
-    <div class="col s12 m4 mt16">
-      <div class="card">
-        <div class="card-image">
-          <img class='responsive-img' src="{{ asset('images/dummy_product_3.jpg')}}">
-          {{-- <span class="card-title">Card Title</span> --}}
-        </div>
-        <div class="card-content">
-          <h5> Card Title</h5>
-          <p>I am a very simple card. I am good at containing small bits of information.</p>
-        </div>
-        <div class="card-action ">
-          <div class='row mb0'>
-            <div class='col s6'>
-              <h5 class='center-align'> <em>$15.99</em></h5>
-            </div>
-            <div class='col s6'>
-              <h5 class='center-align'>
-                <a href="#" title='Add to Cart'>
-                  <i class='material-icons cust_fz36'>add_shopping_cart</i>
-                </a>
-              </h5>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="col s12 m4 mt16">
-      <div class="card">
-        <div class="card-image">
-          <img class='responsive-img' src="{{ asset('images/dummy_product_1.jpg')}}">
-          {{-- <span class="card-title">Card Title</span> --}}
-        </div>
-        <div class="card-content">
-          <h5> Card Title</h5>
-          <p>I am a very simple card. I am good at containing small bits of information.</p>
-        </div>
-        <div class="card-action ">
-          <div class='row mb0'>
-            <div class='col s6'>
-              <h5 class='center-align'> <em>$15.99</em></h5>
-            </div>
-            <div class='col s6'>
-              <h5 class='center-align'>
-                <a href="#" title='Add to Cart'>
-                  <i class='material-icons cust_fz36'>add_shopping_cart</i>
-                </a>
-              </h5>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="col s12 m4 mt16">
-      <div class="card">
-        <div class="card-image">
-          <img class='responsive-img' src="{{ asset('images/dummy_product_2.jpg')}}">
-          {{-- <span class="card-title">Card Title</span> --}}
-        </div>
-        <div class="card-content">
-          <h5> Card Title</h5>
-          <p>I am a very simple card. I am good at containing small bits of information.</p>
-        </div>
-        <div class="card-action ">
-          <div class='row mb0'>
-            <div class='col s6'>
-              <h5 class='center-align'> <em>$15.99</em></h5>
-            </div>
-            <div class='col s6'>
-              <h5 class='center-align'>
-                <a href="#" title='Add to Cart'>
-                  <i class='material-icons cust_fz36'>add_shopping_cart</i>
-                </a>
-              </h5>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="col s12 m4 mt16">
-      <div class="card">
-        <div class="card-image">
-          <img class='responsive-img' src="{{ asset('images/dummy_product_3.jpg')}}">
-          {{-- <span class="card-title">Card Title</span> --}}
-        </div>
-        <div class="card-content">
-          <h5> Card Title</h5>
-          <p>I am a very simple card. I am good at containing small bits of information.</p>
-        </div>
-        <div class="card-action ">
-          <div class='row mb0'>
-            <div class='col s6'>
-              <h5 class='center-align'> <em>$15.99</em></h5>
-            </div>
-            <div class='col s6'>
-              <h5 class='center-align'>
-                <a href="#" title='Add to Cart'>
-                  <i class='material-icons cust_fz36'>add_shopping_cart</i>
-                </a>
-              </h5>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="col s12 m4 mt16">
-      <div class="card">
-        <div class="card-image">
-          <img class='responsive-img' src="{{ asset('images/dummy_product_1.jpg')}}">
-          {{-- <span class="card-title">Card Title</span> --}}
-        </div>
-        <div class="card-content">
-          <h5> Card Title</h5>
-          <p>I am a very simple card. I am good at containing small bits of information.</p>
-        </div>
-        <div class="card-action ">
-          <div class='row mb0'>
-            <div class='col s6'>
-              <h5 class='center-align'> <em>$15.99</em></h5>
-            </div>
-            <div class='col s6'>
-              <h5 class='center-align'>
-                <a href="#" title='Add to Cart'>
-                  <i class='material-icons cust_fz36'>add_shopping_cart</i>
-                </a>
-              </h5>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="col s12 m4 mt16">
-      <div class="card">
-        <div class="card-image">
-          <img class='responsive-img' src="{{ asset('images/dummy_product_2.jpg')}}">
-          {{-- <span class="card-title">Card Title</span> --}}
-        </div>
-        <div class="card-content">
-          <h5> Card Title</h5>
-          <p>I am a very simple card. I am good at containing small bits of information.</p>
-        </div>
-        <div class="card-action ">
-          <div class='row mb0'>
-            <div class='col s6'>
-              <h5 class='center-align'> <em>$15.99</em></h5>
-            </div>
-            <div class='col s6'>
-              <h5 class='center-align'>
-                <a href="#" title='Add to Cart'>
-                  <i class='material-icons cust_fz36'>add_shopping_cart</i>
-                </a>
-              </h5>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="col s12 m4 mt16">
-      <div class="card">
-        <div class="card-image">
-          <img class='responsive-img' src="{{ asset('images/dummy_product_1.jpg')}}">
-          {{-- <span class="card-title">Card Title</span> --}}
-        </div>
-        <div class="card-content">
-          <h5> Card Title</h5>
-          <p>I am a very simple card. I am good at containing small bits of information.</p>
-        </div>
-        <div class="card-action ">
-          <div class='row mb0'>
-            <div class='col s6'>
-              <h5 class='center-align'> <em>$15.99</em></h5>
-            </div>
-            <div class='col s6'>
-              <h5 class='center-align'>
-                <a href="#" title='Add to Cart'>
-                  <i class='material-icons cust_fz36'>add_shopping_cart</i>
-                </a>
-              </h5>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+  @endforeach
 
 @endsection
