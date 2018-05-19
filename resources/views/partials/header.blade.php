316ed0aadc2696583491ec907dfe896664ca451d
Toufiq <burningraven06@gmail.com>
2018-05-19 15:02:20 +0600

    use icons

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,9 +2,28 @@
   <div class="nav-wrapper">
     <a href="/" class="brand-logo">LRVL Shop</a>
     <ul id="nav-mobile" class="right hide-on-med-and-down">
-      <li><a href="">Sass</a></li>
-      <li><a href="">Components</a></li>
-      <li><a href="">JavaScript</a></li>
+      <li>
+        <a href="#!" class='dropdown-trigger' data-target="user_menuitems_dropdown" title="Settings">
+          <i class='material-icons'> person</i>
+        </a>
+      </li>
+      <li>
+        <a href="#!" title="Cart">
+          <i class='material-icons'>shopping_cart</i>
+        </a>
+      </li>
     </ul>
   </div>
 </nav>
+
+<ul id='user_menuitems_dropdown' class='dropdown-content'>
+  <li> <a href="#"> Profile </a></li>
+  <li> <a href="#"> Logout </a></li>
+</ul>
+
+
+@section('scripts')
+<script>
+   $('.dropdown-trigger').dropdown();
+</script>
+@endsection
