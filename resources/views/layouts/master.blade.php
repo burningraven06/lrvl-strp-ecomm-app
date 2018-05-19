0a341c9411d5e90c8443094d28b7c5d89ed856c3
Toufiq <burningraven06@gmail.com>
2018-05-19 14:26:22 +0600

    create nav

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,13 +4,20 @@
     <meta charset='utf-8' />
     <title> @yield('title') </title>
     <link rel='stylesheet' href=' {{ asset('materialize/materialize.min.css') }}' />
-    <script src='{{ asset('materialize/materialize.min.js')}}'> </script>
+    <link rel='stylesheet' href=' {{ asset('materialize/materialize-icons.css')}}' />
     @yield('styles')
   </head>
+
   <body>
+    @include('partials.header')
     <div class='container'>
       @yield('content')
     </div>
   </body>
+
+  <script src='{{ asset('jquery3/jquery.3.min.js')}}'> </script>
+  <script src='{{ asset('materialize/materialize.min.js')}}'> </script>
+
   @yield('scripts')
+
 </html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,10 @@
+<nav>
+  <div class="nav-wrapper">
+    <a href="/" class="brand-logo">LRVL Shop</a>
+    <ul id="nav-mobile" class="right hide-on-med-and-down">
+      <li><a href="">Sass</a></li>
+      <li><a href="">Components</a></li>
+      <li><a href="">JavaScript</a></li>
+    </ul>
+  </div>
+</nav>
