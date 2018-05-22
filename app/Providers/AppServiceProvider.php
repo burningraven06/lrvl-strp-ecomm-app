a113a184b7cb8a4b7571fbac9a30b519d31ac63e
Toufiq <burningraven06@gmail.com>
2018-05-22 16:18:09 +0600

    use new function name for vresion upgrade

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,7 +17,7 @@ class AppServiceProvider extends ServiceProvider
     {
       if(env('APP_ENV') !== 'local')
         {
-            $url->forceSchema('https');
+            $url->forceScheme('https');
         }
     }
 
